d6ce3d7b368a37046c22493fa7a6362ef1eb5a5c
Cloraxlan <sbluek546@gmail.com>
2020-10-22 00:00:37 -0500

    added cors

--- index.ts
+++ index.ts
@@ -5,11 +5,13 @@ import hhs from "./routes/hhs";
 import SpreadSheetSchedual from "./processes/SpreadSheetSchedual";
 import { DateTime } from "luxon";
 import hhsDay from "./routes/hhsDay";
+var cors = require('cors')
+
 //Creates express server
 let app = Express();
 
 require("dotenv").config();
-
+app.use(cors)
 app.set("views", path.join(__dirname, "views"));
 //Sets html as rendering engine
 app.engine("html", require("ejs").renderFile);
