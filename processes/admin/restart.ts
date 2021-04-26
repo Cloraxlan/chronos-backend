e480c47cbfc05d4a78dc40ebf75fbabe71241151
Konrad Rozpadek <sbluek546@gmail.com>
2021-04-26 08:26:15 -0500

    restart

--- processes/admin/restart.ts
+++ processes/admin/restart.ts
@@ -3,6 +3,9 @@ import { connection } from "../MongoConnection";
 
 export default Router().get("/", async (req, res) => {
   res.send("success");
-
-  process.exit();
+  const Heroku = require("heroku-client");
+  const heroku = new Heroku({ token: "0a336d67-744a-4784-b9bc-17af19c42fb9" });
+  heroku.delete("/apps/chronoshhs/dynos/web").then((app: any) => {
+    console.log(app);
+  });
 });
